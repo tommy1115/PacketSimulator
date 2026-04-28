2dc04f08cc969ed87d0835a71342734a92b5eae0
ethan.hou <ethan,hou@gtbooster.com>
2026-04-28 15:26:25 +0800

    fix the monitor cpu an ram

--- ts004_packet_simulator_onelink.bat
+++ ts004_packet_simulator_onelink.bat
@@ -31,7 +31,8 @@ set target_name=%5
 set round=0
 set file_path=new_file.txt
 set csvfile=out.csv
-echo "Test","Target","Version","Round","CPU_min","CPU_max","RAM_min(B)","RAM_max(B)","Timestamp","Duration","SCRIPT_FILENAME","APPL_SCRIPT_NAME","Protocol","E1_ADDR","E2_ADDR","CONSECUTIVE_LOST","CPU_UTIL_E1","CPU_UTIL_E2","DELAY_FACTOR","DELAY_VARIATION","END_TO_END_DELAY","JITTER_AVG","JITTER_MIN","JITTER_MAX","MEDIA_LOSS_RATE","MOS_ESTIMATE_AVG","MOS_ESTIMATE_MIN","MOS_ESTIMATE_MAX","ONE_WAY_DELAY_AVG","ONE_WAY_DELAY_MIN","ONE_WAY_DELAY_MAX","R_VALUE_AVG","R_VALUE_MIN","R_VALUE_MAX","REL_PRECISION","RESP_TIME_AVG","RESP_TIME_MIN","RESP_TIME_MAX","RSSI_E1_AVG","RSSI_E1_MIN","RSSI_E1_MAX","RSSI_E2_AVG","RSSI_E2_MIN","RSSI_E2_MAX","THROUGHPUT_AVG","THROUGHPUT_MIN","THROUGHPUT_MAX","TRANS_RATE_AVG","TRANS_RATE_MIN","TRANS_RATE_MAX" >> %csvfile%
+echo "Target","Version","Round","CPU_before","CPU_during","CPU_after","RAM_before","RAM_during","RAM_after" >> %csvfile%
+@REM echo "Test","Target","Version","Round","CPU_min","CPU_max","RAM_min(B)","RAM_max(B)","Timestamp","Duration","SCRIPT_FILENAME","APPL_SCRIPT_NAME","Protocol","E1_ADDR","E2_ADDR","CONSECUTIVE_LOST","CPU_UTIL_E1","CPU_UTIL_E2","DELAY_FACTOR","DELAY_VARIATION","END_TO_END_DELAY","JITTER_AVG","JITTER_MIN","JITTER_MAX","MEDIA_LOSS_RATE","MOS_ESTIMATE_AVG","MOS_ESTIMATE_MIN","MOS_ESTIMATE_MAX","ONE_WAY_DELAY_AVG","ONE_WAY_DELAY_MIN","ONE_WAY_DELAY_MAX","R_VALUE_AVG","R_VALUE_MIN","R_VALUE_MAX","REL_PRECISION","RESP_TIME_AVG","RESP_TIME_MIN","RESP_TIME_MAX","RSSI_E1_AVG","RSSI_E1_MIN","RSSI_E1_MAX","RSSI_E2_AVG","RSSI_E2_MIN","RSSI_E2_MAX","THROUGHPUT_AVG","THROUGHPUT_MIN","THROUGHPUT_MAX","TRANS_RATE_AVG","TRANS_RATE_MIN","TRANS_RATE_MAX" >> %csvfile%
 
 REM Modify down below
 
@@ -50,7 +51,7 @@ set "StartTime=%T_DATETIME:~0,14%"
 echo Packet_Simulator_Start_Time=%StartTime%
 
 REM TODO(REQ): script and protocol change to array[]
-REM TODO(REQ): 紀錄 CPU, RAM 在執行前中後(執行中要定期取值並取得最大值)
+REM TODO(REQ): 紀錄 CPU, RAM 在執行前中後(執行中要定期取值並取得最大值) (還沒測試)
 
     REM ===== 執行前（取樣 5 秒）=====
     for /L %%i in (1,1,5) do (
@@ -58,16 +59,17 @@ REM TODO(REQ): 紀錄 CPU, RAM 在執行前中後(執行中要定期取值並取
         for /f "skip=2 tokens=2 delims=," %%d in ('typeperf "\Process(!target_name!)\%% Processor Time" -sc 1') do (
             set "cpu=%%d"
             set "cpu=!cpu:~1,8!"
-            set cpu_int=!cpu:.=!
+            for /f "tokens=1 delims=." %%x in ("!cpu!") do set cpu_int=%%x
         )
         if !cpu_int! gtr !cpu_max_before! set cpu_max_before=!cpu_int!
 
         for /f "skip=2 tokens=2 delims=," %%e in ('typeperf "\Process(!target_name!)\Working Set - Private" -sc 1') do (
-            set "ram=%%e"
-            set "ram=!ram:~1,8!"
-            set ram_int=!ram!
+            set "ram_raw=%%e"
+            set "ram_raw=!ram_raw:~1!"
         )
-        if !ram_int! gtr !ram_max_before! set ram_max_before=!ram_int!
+        set /a ram_mb=!ram_raw!/1024/1024
+
+        if !ram_mb! gtr !ram_max_before! set ram_max_before=!ram_mb!
 
         timeout /t 1 >nul
     )
@@ -77,12 +79,14 @@ for /f %%b in ('powershell -NoProfile -Command "Get-Date -Format yyyyMMddHHmmss"
     echo #############################################################################
     set "T_DATETIME=%%b"
     SET T_DATETIME=!T_DATETIME:~0,14%!
+    set "HTTP_SCR_PATH=%CD%\Ixia\IxChariot\Scripts\Internet\HTTPtext.scr"
+
     set duration=3600
     set interval=10
     set /a loops=duration/interval
-    set "HTTP_SCR_PATH=%CD%\Ixia\IxChariot\Scripts\Internet\HTTPtext.scr"
+    
 
-    start "" /b tclsh TS004_OneLink_OnePair.tcl %1 %2 "!HTTP_SCR_PATH!" TCP 30 "!T_DATETIME!" 2>> nul
+    start "" /b tclsh TS004_OneLink_OnePair.tcl %1 %2 "!HTTP_SCR_PATH!" TCP !duration! "!T_DATETIME!" 2>> nul
     
     REM 1. processor time 2. private working set
 
@@ -93,17 +97,18 @@ for /f %%b in ('powershell -NoProfile -Command "Get-Date -Format yyyyMMddHHmmss"
         for /f "skip=2 tokens=2 delims=," %%d in ('typeperf "\Process(!target_name!)\%% Processor Time" -sc 1') do (
             set "cpu=%%d"
             set "cpu=!cpu:~1,8!"
-            set cpu_int=!cpu:.=!
+            for /f "tokens=1 delims=." %%x in ("!cpu!") do set cpu_int=%%x
         )
         if !cpu_int! gtr !cpu_max_during! set cpu_max_during=!cpu_int!
 
         REM ---- RAM ----
         for /f "skip=2 tokens=2 delims=," %%e in ('typeperf "\Process(!target_name!)\Working Set - Private" -sc 1') do (
-            set "ram=%%e"
-            set "ram=!ram:~1,8!"
-            set ram_int=!ram!
+            set "ram_raw=%%e"
+            set "ram_raw=!ram_raw:~1!"
         )
-        if !ram_int! gtr !ram_max_during! set ram_max_during=!ram_int!
+        set /a ram_mb=!ram_raw!/1024/1024
+
+        if !ram_mb! gtr !ram_max_during! set ram_max_during=!ram_mb!
 
         REM ---- 每10秒抓一次 ----
         timeout /t !interval! >nul
@@ -115,16 +120,17 @@ for /f %%b in ('powershell -NoProfile -Command "Get-Date -Format yyyyMMddHHmmss"
         for /f "skip=2 tokens=2 delims=," %%d in ('typeperf "\Process(!target_name!)\%% Processor Time" -sc 1') do (
             set "cpu=%%d"
             set "cpu=!cpu:~1,8!"
-            set cpu_int=!cpu:.=!
+            for /f "tokens=1 delims=." %%x in ("!cpu!") do set cpu_int=%%x
         )
         if !cpu_int! gtr !cpu_max_after! set cpu_max_after=!cpu_int!
 
         for /f "skip=2 tokens=2 delims=," %%e in ('typeperf "\Process(!target_name!)\Working Set - Private" -sc 1') do (
-            set "ram=%%e"
-            set "ram=!ram:~1,8!"
-            set ram_int=!ram!
+            set "ram_raw=%%e"
+            set "ram_raw=!ram_raw:~1!"
         )
-        if !ram_int! gtr !ram_max_after! set ram_max_after=!ram_int!
+        set /a ram_mb=!ram_raw!/1024/1024
+
+        if !ram_mb! gtr !ram_max_after! set ram_max_after=!ram_mb!
 
         timeout /t 1 >nul
     )
@@ -134,7 +140,6 @@ for /f %%b in ('powershell -NoProfile -Command "Get-Date -Format yyyyMMddHHmmss"
     "!cpu_max_before!","!cpu_max_during!","!cpu_max_after!", ^
     "!ram_max_before!","!ram_max_during!","!ram_max_after!" >> %csvfile%
     
-    del !file_path!
     echo ## !T_DATETIME!, TCP, "!HTTP_SCR_PATH!" --------------------------------------
     timeout /t 10
 )
